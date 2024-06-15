6e7dbc0c19c6cac4f9a48efd37b27408466b332c
Dady-m <22KM153@esisalama.org>
2024-06-15 10:46:38 +0200

    fonction pour executer le menu principale

---
+++ fonctinmenuprincipale.c
@@ -0,0 +1,9 @@
+void afficherMenu() {
+    printf("\nMenu Principal :\n");
+    printf("1. Afficher les itineraires disponibles\n");
+    printf("2. Reserver un billet\n");
+    printf("3. Rechercher un passager et ses reservations\n");
+    printf("4. Afficher les bus par itineraire\n");
+    printf("5. Payer une reservation\n");
+    printf("6. Quitter\n");
+}
